9d6e3a6db62a7d0a8e2df46a562f0ad439f5bb8c
olgasouto <77149445+olgasouto@users.noreply.github.com>
2021-01-22 18:50:47 +0100

    Ejercicio2

---
+++ Ejercicio 2/index.js
@@ -0,0 +1,12 @@
+console.log('hola');
+const olga= {
+    day: 20,
+    month: 'febrero'
+}
+console.log('Mi cumpleaños es el',olga.day,'de',olga.month);
+
+//Segundo apartado
+const meses=['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre',
+'octubre','noviembre','diciembre'];
+
+console.log(meses[2]);
