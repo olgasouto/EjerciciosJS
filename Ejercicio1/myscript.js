884c4898126574a86fabd895bc2525e991609cea
olgasouto <77149445+olgasouto@users.noreply.github.com>
2021-01-22 18:14:52 +0100

    Ejercicio1

---
+++ Ejercicio1/myscript.js
@@ -0,0 +1,18 @@
+'use strict'
+alert('El primer código en JavaScript');
+alert('Hola Olga');
+const volumen=16*23*13;
+console.log('volumen',volumen + ' cm3');
+const areabase=16*23;
+const arealateral=23*13
+const areatotal=arealateral+2*areabase
+console.log('areatotal',areatotal + ' cm2');
+
+//Generalización
+const largo=23;
+const ancho=16;
+const altura=13;
+const volumenprismarect=largo*ancho*altura;
+const areaprismarect=largo*altura+2*largo*ancho;
+console.log('volumenprismarect',volumenprismarect);
+console.log('areaprismarect',areaprismarect);
